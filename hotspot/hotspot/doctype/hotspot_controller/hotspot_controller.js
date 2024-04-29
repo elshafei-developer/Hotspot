cb05273eebb51384003a494bde5f2063030b995e
elshafei-developer <el.shafei.developer@gmail.com>
2024-04-29 14:14:47 +0300

    add inner to check connect hotspot

--- hotspot/hotspot/doctype/hotspot_controller/hotspot_controller.js
+++ hotspot/hotspot/doctype/hotspot_controller/hotspot_controller.js
@@ -2,6 +2,30 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Hotspot Controller", {
-  //   refresh(frm) {
-  //   },
+  onload(frm) {
+    frm.set_intro("The Hotspot connection is checked...", "blue");
+    frappe
+      .call({
+        method:
+          "hotspot.hotspot.doctype.vouchers.rest_api_hotspot.connect_hotspot",
+        args: {
+          method: "GET",
+        },
+      })
+      .then((r) => {
+        frm.refresh();
+        if (r.message) {
+          if (r.message == "ERROR") {
+            frm.set_intro(__("You are not authorized to this Hotspot"), "red");
+          } else {
+            frm.set_intro(__("Successfully Connected to the Hotspot"), "green");
+          }
+        } else {
+          frm.set_intro(__("The Hotspot is Not Connected"), "red");
+        }
+      });
+  },
+  after_save(frm) {
+    frm.reload_doc();
+  },
 });
