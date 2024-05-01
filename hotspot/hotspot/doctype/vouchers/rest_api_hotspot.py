4fac6eece46b864446d29aa9d6e0af6bf839fb3b
elshafei-developer <el.shafei.developer@gmail.com>
2024-05-01 16:41:46 +0300

    Clean rest_api_hotspot.py

--- hotspot/hotspot/doctype/vouchers/rest_api_hotspot.py
+++ hotspot/hotspot/doctype/vouchers/rest_api_hotspot.py
@@ -5,29 +5,34 @@ import requests
 def connect_hotspot(method,data=None,voucher=None):
 	hotspot_controller = frappe.get_single('Hotspot Controller')
 	ip = hotspot_controller.ip
-
+	admin = hotspot_controller.user
+	password = hotspot_controller.get_password()
+	value_hotspot = {
+		"ip":ip,
+		"admin":admin,
+		"password":password
+    }
 	if method == 'GET':
-		return GET(ip,data)
+		return GET(value_hotspot,data)
 	if method == 'PUT':
-		return PUT(ip,data)
+		return PUT(value_hotspot,data)
 	if method == 'DELETE':
-		return DELETE(ip,data)
+		return DELETE(value_hotspot,data)
 	if method == 'PATCH':
-		return PATCH(ip,data,voucher)
+		return PATCH(value_hotspot,data,voucher)
 
-def url(method,url,data=None):
-    hotspot_controller = frappe.get_single('Hotspot Controller')
-    ip = hotspot_controller.ip
-    admin = hotspot_controller.user
-    password = hotspot_controller.get_password()
-    if data:
-        return requests.request(method,f"http://{ip}/rest/ip/hotspot/{url}",auth=(admin,password),json=data,verify=False)
-    return requests.request(method,f"http://{ip}/rest/ip/hotspot/{url}",auth=(admin,password),verify=False)
+def url(method,url,value_hotspot,data=None):
+    ip = value_hotspot['ip']
+    admin = value_hotspot['admin']
+    password = value_hotspot['password']
+	
+    return requests.request(method,f"http://{ip}/rest/ip/hotspot/{url}",auth=(admin,password),json=data,verify=False)
 
-def GET(ip,name=None):
+def GET(value_hotspot,name=None):
+	ip = value_hotspot['ip']
 	if name:
 		try:
-			api = url("GET",f"user/{name}")
+			api = url("GET",f"user/{name}",value_hotspot)
 			if api.status_code == 200:
 				return api.json()
 			else:
@@ -38,9 +43,9 @@ def GET(ip,name=None):
 			return False
 	else:
 		try:
-			api = url("GET","user")
+			api = url("GET","user",value_hotspot)
 			if api.status_code == 200:
-				filtered_data = [item for item in api.json() if item.get('dynamic') == 'false' and item.get('name') != 'default-trial']
+				filtered_data = [item for item in api.json() if item.get('dynamic') == 'false' and item.get('default') != 'true']
 				frappe.cache.set_value(f'hotspot{ip}', filtered_data, expires_in_sec=3600)
 				return filtered_data
 			else:
@@ -49,12 +54,13 @@ def GET(ip,name=None):
 			frappe.cache.delete_value(f'hotspot{ip}')
 			return False
 
-def PUT(ip,data):
+def PUT(value_hotspot,data):
+	ip = value_hotspot['ip']
 	if data == None:
 		frappe.throw(_(f"Error: The voucher could not be created Empty."))
 	voucher_exists(data['name'])
 	try:
-		api = url("PUT","user",data)
+		api = url("PUT","user",value_hotspot,data)
 		if api.status_code == 201:
 			frappe.cache.delete_value(f'hotspot{ip}')
 			return True
@@ -65,11 +71,12 @@ def PUT(ip,data):
 		frappe.throw(_(f"Error: => {e}"))
 		return False
 
-def DELETE(ip,voucher):
+def DELETE(value_hotspot,voucher):
+	ip = value_hotspot['ip']
 	if voucher == None:
 		frappe.throw(_(f"Error: A name Voucher must be given to delete the Voucher"))
 	try:
-		api = url("DELETE",f"user/{voucher}")
+		api = url("DELETE",f"user/{voucher}",value_hotspot)
 		if api.status_code == 204:
 			frappe.cache.delete_value(f'hotspot{ip}')
 			frappe.publish_realtime("realtime_vouchers",{"message":"delete"})
@@ -81,12 +88,13 @@ def DELETE(ip,voucher):
 		frappe.throw(_(f"Error: => {e}"))
 		return False
 
-def PATCH(ip,data,voucher):
+def PATCH(value_hotspot,data,voucher):
+	ip = value_hotspot['ip']
 	if voucher == None:
 		frappe.throw(_(f"Error: A name must be given to Update the Voucher"))
 	voucher_exists(data['name'],voucher)
 	try:
-		api = url("PATCH",f"user/{voucher}",data)
+		api = url("PATCH",f"user/{voucher}",value_hotspot,data)
 		if api.status_code == 200:
 			frappe.cache.delete_value(f'hotspot{ip}')
 			return True
@@ -105,5 +113,3 @@ def voucher_exists(new_name,old_name=None):
 		vouchers_exists.pop(vouchers_exists.index(old_name))
 	if new_name in vouchers_exists:
 		frappe.throw(_(f"Error: The voucher '{new_name}' already exists."))
-		
-
